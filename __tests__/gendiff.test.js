4480ee939f18f12f98e1bfc9379cb200ce87eeff
Vladimir Tazenkov <fadefaze@icloud.com>
2023-08-30 12:18:43 +0300

    add tests for .yml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,7 +11,7 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const expectedStylish = readFile('expectedStylish.txt');
 
-test.each(['json'])('%s test', (format) => {
+test.each(['json', 'yml'])('%s test', (format) => {
   const file1 = getFixturePath(`file1.${format}`);
   const file2 = getFixturePath(`file2.${format}`);
   expect(generateDiffs(file1, file2, 'stylish')).toEqual(expectedStylish);
