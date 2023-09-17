aeccc7bee95fdc8096f23ffe440d6ff2fd22704a
Vladimir Tazenkov <fadefaze@icloud.com>
2023-09-17 17:47:59 +0300

    delete unneeded dir

--- src/formats/jsonFormat.js
+++
@@ -1,3 +0,0 @@
-const jsonParse = (content) => JSON.parse(content);
-
-export default jsonParse;
--- src/formats/ymlFormat.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import yaml from 'js-yaml';
-
-const ymlParse = (content) => yaml.load(content);
-
-export default ymlParse;
